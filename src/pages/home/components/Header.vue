8f2aeb273a61786409098625b52cf922fa71ae0c
zihan <nowmeiying@gmail.com>
2019-07-23 12:53:37 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display flex
+    line-height .86rem
+    background $bgColor
+    color #fff
+    .header-left
+      width .64rem
+      float left
+      div
+        text-align center
+        font .4rem
+    .header-input
+      flex 1
+      height .64rem
+      line-height .64rem
+      margin .12rem 0 0 .2rem
+      padding-left .15rem
+      border-redius .1rem
+      background #fff
+      color #ccc
+    .header-right
+      width 1.24rem
+      float right
+      text-align center
+      span
+        margin-left -.04rem
+        font-size .24rem
+</style>
