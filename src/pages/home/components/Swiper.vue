f5d0832e415e428c8e186505aa49d79cb40881c6
zihan <nowmeiying@gmail.com>
2019-07-23 19:08:59 +0800

    swiper finished

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"/>
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 2000
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20196/1a77cfc99bdf951b099c2c963729546b.jpg_890x330_3b8b02bb.jpg'
+        }, {
+          id: '0002',
+          imgUrl: 'https://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20197/852a8c8b82283c30c515808cdc6e64a3.jpg_890x330_8413d1e8.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color #fff
+  .swiper-img
+     width 100%
+</style>
