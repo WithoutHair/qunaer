f2fbff3d6767ede14ec6f5191280c19cdc001d89
zihan <nowmeiying@gmail.com>
2019-07-29 16:05:58 +0800

    列表及其性能优化

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -9,7 +9,7 @@
         <div class="title border-topbottom">热门城市</div>
         <div class="area-city" v-for="item of hot" :key="item.id">{{item.name}}</div>
       </div>
-      <div class="area" v-for="(items, key) of cities" :key="key">
+      <div class="area" v-for="(items, key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">{{key}}</div>
         <div class="all-city" v-for="item of items" :key="item.id">{{item.name}}</div>
       </div>
@@ -23,10 +23,19 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
