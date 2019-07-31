759ad739ba3dc950c68bf0098ca552ce83af9005
zihan <nowmeiying@gmail.com>
2019-07-31 17:58:17 +0800

    banner finished

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container" @click="handleGalleryClose">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide  v-for="(item, index) of imgs" :key="index">
+          <img :src="item"/>
+        </swiper-slide>
+        <div class="swiper-pagination"  slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CommonGallery',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    handleGalleryClose () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container
+    z-index 99
+    display flex
+    flex-direction column
+    justify-content center
+    position fixed
+    top 0
+    right 0
+    bottom 0
+    left 0
+    background #000
+    .wrapper
+      width 100%
+      height 0
+      padding-bottom 100%
+      img
+        width 100%
+    .swiper-pagination
+      color #fff
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="banner" @click="handleShowGallery">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" >
+      <div class="banner-title">故宫(AAAAA景区)</div>
+      <div class="banner-info">
+        <span class="iconfont banner-icon">&#xe757;</span>
+        <span>10</span>
+      </div>
+      <div class="banner-gradient"></div>
+    </div>
+    <CommonGallery @close="handleGalleryClose" :imgs="imgs" v-show="showGallery"></CommonGallery>
+  </div>
+</template>
+
+<script>
+import CommonGallery from 'common/gallery/Gallery'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallery: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1907/cd/cdd8448b61cd6042a3.img.jpg_r_800x800_e8f50aa2.jpg', 'http://img1.qunarzz.com/sight/p0/1907/cd/cdd8448b61cd6042a3.img.jpg_r_800x800_e8f50aa2.jpg']
+    }
+  },
+  components: {
+    CommonGallery
+  },
+  methods: {
+    handleShowGallery () {
+      this.showGallery = true
+    },
+    handleGalleryClose () {
+      this.showGallery = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    color #fff
+    .banner-img
+      width 100%
+    .banner-title
+      position absolute
+      top auto
+      right auto
+      bottom .4rem
+      left .2rem
+      font-size .36rem
+    .banner-info
+      box-sizing border-box
+      position absolute
+      top auto
+      right auto
+      bottom .9rem
+      left .24rem
+      padding .08rem .32rem
+      border-radius .2rem
+      background rgba(0, 0, 0, .4)
+      font-size .24rem
+      .banner-icon
+        font-size .26rem
+    .banner-gradient
+      position absolute
+      bottom 0
+      width 100%
+      min-height .5rem
+      background linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, .8))
+</style>
