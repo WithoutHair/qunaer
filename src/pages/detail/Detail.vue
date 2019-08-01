1ee8ef8f7e5d45b5da69d9b55e4fe9eedb70dcb8
zihan <nowmeiying@gmail.com>
2019-08-01 15:50:37 +0800

    完成header渐变效果

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
     <DetailBanner></DetailBanner>
+    <DetailHeader></DetailHeader>
+    <div class="blank"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
-    DetailBanner
+    DetailBanner,
+    DetailHeader
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+  .blank
+    height 30rem
+</style>
---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <router-link tag="div" to="/" class="header-abs iconfont" v-show="showAbs">&#xe624;</router-link>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <router-link tag="div" to="/" class="header-back iconfont">&#xe624;</router-link>
+      <div class="header-text">景点详情</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleFixedShow () {
+      const top = document.documentElement.scrollTop
+      if (top > 56) {
+        let opacity = top / 164
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = { opacity }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleFixedShow)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header-abs
+    position absolute
+    top .2rem
+    left .2rem
+    width .8rem
+    height .8rem
+    line-height .8rem
+    text-align center
+    border-radius .4rem
+    background rgba(0, 0, 0, .4)
+    color #fff
+  .header-fixed
+    position fixed
+    top 0
+    right 0
+    left 0
+    line-height $headerHeight
+    background-color $bgColor
+    color #fff
+    font-size .34rem
+    .header-text
+      text-align center
+    .header-back
+      position absolute
+      left .1rem
+</style>
